14e4d023ea0a67ce0e920a282ce162c26b622ccf
Auryx <lilbizb1@gmail.com>
2023-05-18 11:20:26 -0700

    Commit 3 - I added list elements. That code didn't seem very dry. I wonder if there is something I could do about it in the Hungry for More section

--- hogwarts/app.js
+++ hogwarts/app.js
@@ -4,9 +4,7 @@
 $(() => {
     // Year 1
     const $container = $('#container')
-	console.log($container)
     const $h1 = $('<h1>')
-    console.log($h1)
     $h1.text('Hogwarts')
     $('#container').append($h1)
 
@@ -15,7 +13,23 @@ $(() => {
     const $house = $('<h3>').text('Ravenclaw')
     const $pet = $('<h4>').text('Leopold').addClass('axolotl')
     const $wand = $('<h4>').text('Holly Wand with Unicorn Hair Core')
-    
-
     $('#container').append($name, $house, $pet, $wand)
+
+    // Year 3
+    const $ul = $('<ul>').attr('storage', 'trunk')
+    const $butterBeer = $("<li>").text("Butter Beer");
+    const $invCloak = $("<li>").text("Invisibility Cloak");
+    const $magicMap = $("<li>").text("Magic Map");
+    const $timeTurner = $("<li>").text("Invisibility Cloak");
+    const $leash = $("<li>").text("Leash").addClass('axolotl');
+    const $beans = $("<li>").text("Bertie Bott's Every Flavor [Jelly] Beans");
+    
+    //was getting errors if I tried to do addClass on the text line
+    $invCloak.addClass('secret')
+    $magicMap.addClass('secret')
+    $timeTurner.addClass('secret')
+
+    $ul.append($butterBeer, $invCloak, $magicMap, $timeTurner, $leash, $beans)
+    console.dir($ul)
+    $('#container').append($ul)
 });
