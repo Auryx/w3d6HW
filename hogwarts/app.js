0579f82e4d369bcb274b35ef1c549a2405192efa
Auryx <lilbizb1@gmail.com>
2023-05-18 10:59:00 -0700

    Commit 2 - I added more elements. My power grows with my dedication and hard work

--- hogwarts/app.js
+++ hogwarts/app.js
@@ -2,10 +2,20 @@
 //     console.log('oops! I still have to link my jQuery properly!');
 //   } else {console.log('I did it! I linked jQuery and this js file!')};
 $(() => {
-    let $container = $('#container')
+    // Year 1
+    const $container = $('#container')
 	console.log($container)
-    let $h1 = $('<h1>')
+    const $h1 = $('<h1>')
     console.log($h1)
     $h1.text('Hogwarts')
     $('#container').append($h1)
+
+    // Year 2
+    const $name = $('<h2>').text('Paul Bunda')
+    const $house = $('<h3>').text('Ravenclaw')
+    const $pet = $('<h4>').text('Leopold').addClass('axolotl')
+    const $wand = $('<h4>').text('Holly Wand with Unicorn Hair Core')
+    
+
+    $('#container').append($name, $house, $pet, $wand)
 });
